4d108535e39ecd3cc038581dc7d0c7e372f081ae
myreads <meerabmadeem20@gmail.com>
2026-05-18 16:52:23 +0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById("signupForm");
-
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  alert("Your wedding invitation request has been submitted successfully!");
-  form.reset();
-});
